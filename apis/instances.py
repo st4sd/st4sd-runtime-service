d8bc07aa86f769fec357af9a1b57e4b7941fcba4
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2023-06-10 11:16:32 +0100

    chore: fix typo
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- apis/instances.py
+++ apis/instances.py
@@ -180,7 +180,7 @@ def get_list_instances(
                 identifier = (package_name, digest)
                 cache_experiments[identifier] = None
 
-        current_app.logger.info(f"There are {len(item)} instances which are associated with {len(cache_experiments)} "
+        current_app.logger.info(f"There are {len(items)} instances which are associated with {len(cache_experiments)} "
                                 f"parameterised virtual experiment packages")
 
         with database:
